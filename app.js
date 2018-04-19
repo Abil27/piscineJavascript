8be5700c64f0829b24337d8580229c6f6d0452c5
Ahssimi mamadou Housseini <ahoussei@e1z1r4p30.42.us.org>
2018-04-18 23:50:14 -0700

    different data types in JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-alert("Hello world!");
+// primitive data types
+
+// string
+
+const name = 'houssein abil';
+console.log(typeof name);
+
+// number
+const number = 42;
+console.log(typeof number);
+
+// boolean
+const married = true;
+console.log(typeof married);
+
+//null
+const playing = null;
+console.log(typeof playing);
+
+//undefined
+let eating;
+console.log(typeof eating);
+
+//symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+// REFERENCE TYPE - return all objects
+
+// array
+const hobbies = ['coding', 'eating', 'sleeping'];
+console.log(typeof hobbies);
+
+//object literals
+const address = {
+    city: 'Fremont',
+    state: 'California'
+};
+console.log(typeof address);
+
+//date
+const today = new Date();
+console.log(typeof today);
