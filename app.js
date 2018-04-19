8919a1ec15493d0ee1a6169e9e3bfe2e83c91fcb
Ahssimi mamadou Housseini <ahoussei@e1z1r4p30.42.us.org>
2018-04-19 00:31:10 -0700

    different type of conversion in JavaScript (converting string to number or number to string)

--- app.js
+++ app.js
@@ -41,3 +41,34 @@ console.log(typeof address);
 //date
 const today = new Date();
 console.log(typeof today);
+
+// converting number to string
+let val;
+
+val = String(42);
+console.log(val);
+
+//converting boolean to string
+val = String(true);
+console.log(val);
+
+//converting date to string
+val = String(new Date());
+console.log(val);
+
+//we also have the toString() method
+// converting array to string
+val = ([1, 2, 3, 4]).toString();
+console.log(val);
+
+//NOW LETS CONVERT STRING TO NUMBER
+val = Number('5');
+console.log(val);
+
+// or we can use the parseInt() method. works only on nombre entier
+val = parseInt('42');
+console.log(val);
+
+//transform decimal string to Number
+val = parseFloat('42.43');
+console.log(val);
