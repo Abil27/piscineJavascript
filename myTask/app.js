ee38a43c3bfb421418605d29579a30b64ec64365
Ahssimi mamadou Housseini <ahoussei@e1z1r4p30.42.us.org>
2018-04-20 19:02:27 -0700

    create loadevent listener function, add task function

--- myTask/app.js
+++ myTask/app.js
@@ -4,3 +4,45 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+//load all Event listeners
+loadEventListeners();
+
+// function to load all event Listeners
+function loadEventListeners() {
+    // add task event listener
+    form.addEventListener('submit', addTask);
+}
+
+// function add Task
+function addTask(event) {
+    if(taskInput.value === ''){
+        alert('Dude add task');
+    }
+
+
+    // create li element when task is added
+    const li = document.createElement('li');
+    // add class to the li of the added task
+    li.className = 'collection-item';
+    // create text node and append it to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a');
+    // add class to the link element
+    link.className = 'delete-item secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append the link to the li
+    li.appendChild(link);
+    // append the li to the ul
+    taskList.appendChild(li);
+    // clear input after we submit
+    taskInput.value = '';
+
+
+
+
+
+    event.preventDefault();
+}
